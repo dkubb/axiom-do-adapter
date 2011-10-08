fdce61247c656a37d9c2ff44bc70bf0eccae77fe
Dan Kubb <dan.kubb@gmail.com>
2011-10-07 23:18:04 -0700

    Fix whitespace

--- spec/support/example_group_methods.rb
+++ spec/support/example_group_methods.rb
@@ -1,6 +1,6 @@
 module Spec
   module ExampleGroupMethods
-    def testing_block_passing_broken?                                               
+    def testing_block_passing_broken?
       RUBY_PLATFORM.include?('java') && JRUBY_VERSION <= '1.6.4' && RUBY_VERSION >= '1.9.2'
     end
   end
