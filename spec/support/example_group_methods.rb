e98bf5f89eda63e3e1350fbf6ef2fff791d3450f
Dan Kubb <dan.kubb@gmail.com>
2011-11-03 15:58:00 -0700

    Fix spec helper method to skip specs under JRuby <= 1.6.5 in 1.9 mode

--- spec/support/example_group_methods.rb
+++ spec/support/example_group_methods.rb
@@ -1,7 +1,7 @@
 module Spec
   module ExampleGroupMethods
     def testing_block_passing_broken?
-      RUBY_PLATFORM.include?('java') && JRUBY_VERSION <= '1.6.4' && RUBY_VERSION >= '1.9.2'
+      RUBY_PLATFORM.include?('java') && JRUBY_VERSION <= '1.6.5' && RUBY_VERSION >= '1.9.2'
     end
   end
 end
