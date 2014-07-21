621776ec2225c756097f30b42cc532b574ace741
snusnu <gamsnjaga@gmail.com>
2014-07-21 17:41:34 +0200

    Allow to proxy Relation#one in Gateway

--- lib/axiom/relation/gateway.rb
+++ lib/axiom/relation/gateway.rb
@@ -9,7 +9,7 @@ module Axiom
       DECORATED_CLASS = superclass
 
       # remove methods so they can be proxied
-      undef_method(*DECORATED_CLASS.public_instance_methods(false).map(&:to_s) - %w[materialize])
+      undef_method(*DECORATED_CLASS.public_instance_methods(false).map(&:to_s) - %w[materialize one])
       undef_method(:project, :remove, :extend, :rename, :restrict, :sort, :sort_by, :reverse, :drop, :take)
 
       # The adapter the gateway will use to fetch results
